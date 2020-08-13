c15de0fa3149fe6b59d4c0a7abc553f7482f56fa
Alexis Ramirez <alexisr1993@gmail.com>
2020-08-13 00:48:58 -0700

    Remove comments

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-// Use morgan for logging
-// Use joi for validation
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -10,11 +8,9 @@ const routes = require('./api/routes/routes');
 
 const port = process.env.PORT || 4000;
 
-// Mongoose Connection
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/quizdb', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Middleware
 app.use(cors());
 app.use(morgan('tiny'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,7 +19,6 @@ app.use(bodyParser.json());
 // Register routes
 routes(app);
 
-// Default when route not found
 app.use((req, res) => {
   res.status(404).send({ url: `${req.originalUrl} not found` });
 });
